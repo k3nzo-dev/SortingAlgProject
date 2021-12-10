fcf4b5bf160a6149008356c837863cfd135e634c
Lorenzo Boschi <lboschi07@gmail.com>
2021-12-10 13:49:38 -0500

    Added sort option choice

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
+import javax.swing.*;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Runner {
 
@@ -8,19 +8,33 @@ public class Runner {
 
         Integer[] arrayInt = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
                 25};
-        File book = new File("/Users/lboschi25/Desktop/SortingAlgProject/src/allOfHarryPotter1.rtf");
+        File book = new File("/Users/lboschi25/Dropbox/BoschiLorenzo/Projects/SortingAlgProject/src/allOfHarryPotter1.txt");
 
+
+        String[] choices = {"Bubble", "Selection"};
+        String instructions = "What sort method would you like to use?";
+        String title = "Sort Chose";
+        String choice = (String) JOptionPane.showInputDialog(
+                null,
+                instructions, //text in window
+                title, //titlebar
+                JOptionPane.PLAIN_MESSAGE, //the icon type
+                null,
+                choices, //array of options
+                choices[0]); //the default
+
+        if (choice.equalsIgnoreCase("bubble")) {
+            System.out.println(BubSort.bubSort(arrayInt));
+        } else if (choice.equalsIgnoreCase("Selection")) {
+            System.out.println(SelectionSort.selectionSort(arrayInt));
+        }
         JavaPaintUI.uiStart();
-        System.out.println(BubSort.bubSort(arrayInt));
-        System.out.println("");
-        System.out.println(SelectionSort.selectionSort(arrayInt));
-        System.out.println("");
-        ArrayList<String> sortedBook = BookSort.bookSort(book);
-        System.out.println(sortedBook.size());
+        //ArrayList<String> sortedBook = BookSort.bookSort(book);
 
 
     }
 
 
 }
+
 
