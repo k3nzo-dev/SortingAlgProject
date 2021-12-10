84b4967b525a0a8ed9e21ae83a716010c9b1d7d7
Lorenzo Boschi <lboschi07@gmail.com>
2021-12-10 13:48:00 -0500

    Updated Utility
    Added a method to create shuffled ArrayLists up to declared int

--- src/Utility.java
+++ src/Utility.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class Utility {
 
@@ -11,7 +9,7 @@ public class Utility {
         return response.equals("yes") || response.equals("y") || response.equals("yeah");
     }
 
-    public static void sleepIt(int mls){
+    public static void sleepIt(int mls) {
         try {
             Thread.sleep(mls);
         } catch (InterruptedException e) {
@@ -19,19 +17,19 @@ public class Utility {
         }
     }
 
-    public static void popup(String title, String msg){
+    public static void popup(String title, String msg) {
         JOptionPane.showMessageDialog(null, msg,
                 title, JOptionPane.PLAIN_MESSAGE);
     }
 
-    public static Integer[] ShuffledIntArray() {
-        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-                25};
-        List<Integer> intList = Arrays.asList(intArray);
+    public static ArrayList<Integer> ShuffledIntArray(int max) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        for (int i = 0; i <= max; i++) {
+            intList.add(i);
+        }
         Collections.shuffle(intList);
-        intList.toArray(intArray);
 
-        return intArray;
+        return intList;
     }
 
 
